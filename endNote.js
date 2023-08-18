275d755c884a62af3a7c6fcc691084d3fd9605e6
Fynn Schneider <fynnschneider05@gmail.com>
2023-08-18 13:23:22 +0200

    load Json in endNote.js

---
+++ endNote.js
@@ -0,0 +1,16 @@
+LkNoten = JSON.parse(localStorage.getItem("LkNoten"));
+
+mAbiturHalbjahrNoten = JSON.parse(localStorage.getItem("mAbiturHalbjahrNoten"));
+
+FacharbeitNote = JSON.parse(localStorage.getItem("FacharbeitNote"));
+
+
+AbiturNoten = JSON.parse(localStorage.getItem("AbiturNoten"));
+
+console.log("hello")
+
+
+//Berechnung der Note
+function Berechnung(){
+    
+}
